aaf5aa3f3af96278bf7850bd56f246e0c2564b50
Michael <michael.oneill.2020@mumail.ie>
2023-03-05 14:38:33 +0000

    Implement Solver.GetSolution method

--- src/Solution/Solver.cs
+++ src/Solution/Solver.cs
@@ -10,8 +10,13 @@ public static class Solver
     {
         try
         {
-            //Node root1 = enco
-            return false;
+            Node root1 = Parsing.StringToBinaryTree(encoding1);
+            Node root2 = Parsing.StringToBinaryTree(encoding2);
+
+            var fringe1 = root1.GetFringe();
+            var fringe2 = root2.GetFringe();
+
+            return fringe1.SequenceEqual(fringe2, new NodeLabelEqualityComparer());
         }
         catch(Exception e)
         {
