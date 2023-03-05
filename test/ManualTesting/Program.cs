50d4baecf737f9c0640c8139cea0163887687edb
Michael <michael.oneill.2020@mumail.ie>
2023-03-05 14:39:17 +0000

    Test solution

--- test/ManualTesting/Program.cs
+++ test/ManualTesting/Program.cs
@@ -1,4 +1,5 @@
-﻿using DataStructures;
+﻿using Solution;
+using DataStructures;
 using DataStructures.BinaryTree;
 
 namespace ManualTesting;
@@ -17,18 +18,36 @@ public class Program
                 //Console.WriteLine($"Left child encoding:  \"{input.GetLeftChildEncoding()}\"");
                 //Console.WriteLine($"Right child encoding: \"{input.GetRightChildEncoding()}\"\n");
 
-                Node? root = Parsing.StringToBinaryTree(input);
+                //Node? root = Parsing.StringToBinaryTree(input);
 
-                if (root is null)
-                {
-                    Console.WriteLine("Resulting tree is null");
-                }
-                else
-                {
-                    Console.Write("Fringe:");
-                    foreach (var node in root.GetFringe())
-                        Console.Write($" {node.Label}");
-                }
+                //if (root is null)
+                //{
+                //    Console.WriteLine("Resulting tree is null");
+                //}
+                //else
+                //{
+                //    Console.Write("Fringe:");
+                //    foreach (var node in root.GetFringe())
+                //        Console.Write($" {node.Label}");
+                //}
+
+                string[] inputs = input.Split(' ');
+
+                var fringe1 = Parsing.StringToBinaryTree(inputs[0]).GetFringe();
+                var fringe2 = Parsing.StringToBinaryTree(inputs[1]).GetFringe();
+
+                Console.Write("Fringe 1:");
+                foreach(var node in fringe1)
+                    Console.Write($" {node.Label}");
+                Console.WriteLine();
+
+                Console.Write("Fringe 2:");
+                foreach (var node in fringe2)
+                    Console.Write($" {node.Label}");
+                Console.WriteLine();
+
+                Console.WriteLine($"Equal: {Solver.GetSolution(inputs[0], inputs[1])}");
+
             }
             catch(Exception e)
             {
